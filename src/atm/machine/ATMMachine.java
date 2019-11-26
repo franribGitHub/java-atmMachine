9572cb762b0b79e7237755efba21759da89ae2d3
21309 <21309@MININT-HADV8T3.columbushs.com>
2019-11-26 08:49:16 -0500

    done

--- src/atm/machine/ATMMachine.java
+++ src/atm/machine/ATMMachine.java
@@ -9,13 +9,75 @@ package atm.machine;
  *
  * @author mlarrubia
  */
+
+import java.util.Scanner;
+
 public class ATMMachine {
 
     /**
      * @param args the command line arguments
      */
+    
+    private static Scanner kb = new Scanner(System.in);
+    
+    private static final String Username = "user";
+    private static final String Password = "1234";
+    private static int balance = 10000;
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Enter Username: ");
+        String enteredUsername = kb.nextLine();
+        System.out.print("Enter Password: ");
+        String enteredPassword = kb.nextLine();
+        if (enteredUsername.equals(Username) && enteredPassword.equals(Password)) {
+            menu();
+        } else {
+            System.out.println("\nYour account has been temporarily locked.");
+        }
+    }
+    
+    public static void menu() {
+        System.out.println("\n1. Check Account Balance");
+        System.out.println("2. Deposit Checks");
+        System.out.println("3. Withdraw Money");
+        System.out.println("4. Logout");
+        System.out.print("\nEnter a number 1-4 to select: ");
+        int choice = kb.nextInt();
+        if (choice == 1) {
+            checkBalance();
+        } else if (choice == 2) {
+            deposit();
+        } else if (choice == 3) {
+            withdraw();
+        } else {
+            System.out.println("\nLogged Out.");
+        }
+    }
+    
+    public static void checkBalance() {
+        System.out.println("\nAccount Balance is $" + balance);
+        menu();
+    }
+    
+    public static void deposit() {
+        System.out.print("\nHow many checks will be deposited: ");
+        int checks = kb.nextInt();
+        for (int i = 1; i <= checks; i++) {
+            System.out.print("How much is check #" + i + ": ");
+            balance += kb.nextInt();
+        }
+        menu();
+    }
+    
+    public static void withdraw() {
+        System.out.print("\nHow much money will be withdrawn: ");
+        int withdraw = kb.nextInt();
+        if (withdraw > balance) {
+            //
+        } else {
+            balance -= withdraw;
+        }
+        menu();
     }
     
 }
